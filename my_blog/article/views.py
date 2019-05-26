c177e9418df78722d82229a8d400784bcda382b8
kobezhangchenghui <970415380@qq.com>
2019-05-26 17:20:52 +0800

    增加layer

--- my_blog/article/views.py
+++ my_blog/article/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import ArticlePost
 import markdown
+from django.http import HttpResponse
+from .form import ArticlePostForm
+from django.contrib.auth.models import User
 
 
 def article_list(request):
@@ -24,3 +27,39 @@ def article_detail(request, id):
     context = { 'article': article }
     return render(request, 'article/detail.html', context)
 
+def article_create(request):
+    if request.method == "POST":
+        artcle_post_form = ArticlePostForm(data=request.POST)
+        if artcle_post_form.is_valid():
+            new_article = artcle_post_form.save(commit=False)
+            new_article.author = User.objects.get(id=1)
+            new_article.save()
+            return redirect("article:article_list")
+        else:
+            return HttpResponse("表单内容有误，请重新填写。")
+    else:
+        artcle_post_form = ArticlePostForm()
+        context = {'article_post_form': artcle_post_form}
+        return render(request, 'article/create.html', context)
+
+def article_delete(author, id):
+    article = ArticlePost.objects.get(id=id)
+    article.delete()
+    return redirect("article:article_list")
+
+
+def article_update(request, id):
+    article = ArticlePost.objects.get(id=id)
+    if request.method == "POST":
+        article_post_form = ArticlePostForm(data=request.POST)
+        if article_post_form.is_valid():
+            article.title = request.POST['title']
+            article.body = request.POST['body']
+            article.save()
+            return redirect("article:article_detail", id=id)
+        else:
+            return HttpResponse("表单内容有误，请重新填写。")
+    else:
+        article_post_form = ArticlePostForm()
+        context = {'article':article, 'article_post_form':article_post_form}
+        return render(request, 'article/update.html', context)
